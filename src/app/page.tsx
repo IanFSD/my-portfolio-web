c3dd22eab1539c6fca5df17bb8d52ad2f3544d08
IanFSD <156086175+IanFSD@users.noreply.github.com>
2024-01-16 02:35:07 +0100

    first projects draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -34,7 +35,9 @@ export default function Home() {
     </section>
 
     {/* Projects */}
-
+    <section id="projects" className="snap-center">
+      <Projects/>
+      </section>
 
     {/* Contact */}
     </div>
