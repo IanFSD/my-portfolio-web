156883af8dd01133624a3c0e1630378b90773691
IanFSD <156086175+IanFSD@users.noreply.github.com>
2024-01-17 09:23:30 +0100

    123

--- src/app/api/fetchPageInfo.ts
+++ src/app/api/fetchPageInfo.ts
@@ -1,13 +1,13 @@
 import { PageInfo } from "@/app/api/types/typings";
 
 
-let query = encodeURIComponent(`*[_type == "pageInfo"][0]`);
+let query = encodeURIComponent(`*[_type == "pageInfo"]`);
 let URL = `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v1/data/query/${process.env.NEXT_PUBLIC_SANITY_DATASET}?query=${query}`;
 
 export const fetchPageInfo = async ()=> {
     const res = await fetch(URL,{ next: { revalidate: 10 } });
     const data = await res.json();
-    const pageInfo: PageInfo = data.result
-
+    const pageInfo: PageInfo = data
+    console.log('url',URL)
     return pageInfo;
 }
